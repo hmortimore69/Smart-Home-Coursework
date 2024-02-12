5c1f5f14e566449f1811c0d00fc0266a8d99c5a8
harveymortimore <harvey999mortimore@gmail.com>
2024-02-12 14:47:41 +0000

    Pycharm Configs + backend.py made SmartPlug class.

--- backend.py
+++ backend.py
@@ -19,6 +19,25 @@ class SmartPlug:
         return f"------\nSmart Plug\n------\nSwitched On: {self.switchedOn}\nConsumption Rate: {self.consumptionRate}"
 
 
+class SmartDoorBell:
+    def __init__(self):
+        self.switchedOn = False
+        self.streaming = "Amazon"
+
+    def toggleSwitch(self):
+        self.switchedOn = not self.switchedOn
+
+    def setStreaming(self, service: str):
+        while service.lower() not in ["amazon", "apple", "spotify"]:
+            print("Invalid streaming service. Please choose from:\n- Amazon\n- Apple\n- Spotify")
+            service = input("Enter your choice of streaming service: ")
+
+        self.streaming = service.lower().capitalize()
+
+    def __str__(self):
+        return f"------\nSmart Doorbell\n------\nSwitched On: {self.switchedOn}\nStreaming Service: {self.streaming}"
+
+
 def testSmartPlug():
     plug = SmartPlug(45)
     plug.toggleSwitch()
@@ -27,4 +46,11 @@ def testSmartPlug():
     print(plug)
 
 
-testSmartPlug()
+def testSmartDoorBell():
+    doorbell = SmartDoorBell()
+    doorbell.toggleSwitch()
+    doorbell.setStreaming(input("Enter a streaming service: "))
+    print(doorbell)
+
+
+testSmartDoorBell()
