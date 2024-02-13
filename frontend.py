ca001e07597551cbb5696033596ef96d5c917126
hmortimore69 <harvey999mortimore@gmail.com>
2024-02-13 21:38:12 +0000

    Instanced Tkinter GUI.

--- frontend.py
+++ frontend.py
@@ -8,6 +8,7 @@ class SmartHomeSystem:
         self.win.title("Smart Home System")
         self.win.geometry("")
         self.mainFrame = Frame(self.win)
+        self.win.configure(bg='aquamarine')
 
     def run(self):
         self.win.mainloop()
@@ -25,12 +26,13 @@ def setUpHome():
                 index = int(input("Enter the catalog index of the device you'd like to add: "))
 
                 if index == 0:  # Smart Plug
-                    # Ensure a value is entered inside the consumption rate w/o needing to renter an index.
+                    # Ensure a value is entered inside the consumption rate w/o needing to re-enter an index.
                     while True:
                         try:
                             consumption_rate = int(input("Enter the consumption rate of the Smart Plug: "))
                             device = deviceChoices[index](consumption_rate)
                             break
+
                         except ValueError:
                             print("Invalid consumption rate. Please enter a number.")
 
@@ -38,7 +40,7 @@ def setUpHome():
                     device = deviceChoices[index]()
 
                 else:
-                    print("Invalid index. Please enter an appropriate value (0 or 1)")
+                    print("Invalid index. Please enter an appropriate value (0 or 1).")
                     continue
 
                 home.addDevice(device)
@@ -52,8 +54,8 @@ def setUpHome():
 
 def main():
     home = setUpHome()
-
     system = SmartHomeSystem()
+
     system.run()
 
 
